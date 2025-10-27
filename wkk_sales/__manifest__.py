41d052c064531e61d7a0b461ba3eea75e376be83
mdai-odoo <mdai@odoo.com>
2025-10-27 13:08:21 +0400

    [5110121][ADD] wkk_sales: Mandatory customer reference
    Before: Customer reference not required
    After: Customer reference required

---
+++ wkk_sales/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'WKK - Sales',
+    'summary': 'WKK - Sales Extension',
+    'author': 'Odoo PS',
+    'website': 'https://www.odoo.com',
+    'category': 'Sales/Sales',
+    'version': '19.0.1.0.0',
+    'license': 'OEEL-1',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'task_ids': [5110121]
+}
+
