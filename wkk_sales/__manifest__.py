ed16ea2c060fc47aff4c66859243a1abf39fa470
mdai-odoo <mdai@odoo.com>
2026-03-13 12:12:42 +0400

    [6022310][ADD] wkk_project/sale: Extra fields on project & adjust field from sale to project customization
    Fields to manage projects created through templates and propagate information to sales/purchase orders

--- wkk_sales/__manifest__.py
+++ wkk_sales/__manifest__.py
@@ -5,11 +5,11 @@
     'author': 'Odoo PS',
     'website': 'https://www.odoo.com',
     'category': 'Sales/Sales',
-    'version': '19.0.2.0.0',
+    'version': '19.0.2.1.0',
     'license': 'OEEL-1',
     'depends': ['wkk_approvals', 'sale_management'],
     'data': [
         'views/sale_order_views.xml',
     ],
-    'task_ids': [5110121, 5211287]
+    'task_ids': [5110121, 5211287, 6022465]
 }
