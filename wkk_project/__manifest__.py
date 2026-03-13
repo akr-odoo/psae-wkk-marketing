317b7c3e875a3a132c65e712aa6cf076480cc10d
mdai-odoo <mdai@odoo.com>
2026-03-13 13:59:03 +0400

    [6022465][ADD] wkk_project: Stage based task visibility
    Archives tasks based on the stage of the project

---
+++ wkk_project/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "WKK - Project",
+    "summary": "WKK - Project Extension",
+    "author": "Odoo PS",
+    "website": "https://www.odoo.com",
+    "category": "Services/Project",
+    "version": "19.0.1.0.0",
+    "license": "OEEL-1",
+    "depends": ["project"],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+    "task_ids": [6022465],
+}
