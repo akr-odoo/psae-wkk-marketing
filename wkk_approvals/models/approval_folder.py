fefa9ed368f959e675e111727c11c11938a51e87
ayek-odoo <ayek@odoo.com>
2026-05-19 17:22:33 +0400

    [ADD][6140074] wkk_approvals: Add approval folder model Approval folder should be opened in dashboard and then approval categories from it. if approval type is redirect approval, then the window action should be returned

---
+++ wkk_approvals/models/approval_folder.py
@@ -0,0 +1,17 @@
+from odoo import api, fields, models
+
+
+class ApprovalFolder(models.Model):
+    _name = 'approval.folder'
+    _description = 'Approval Folder'
+
+    name = fields.Char(required=True)
+    description = fields.Char()
+    company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
+    category_ids = fields.One2many('approval.category', 'folder_id')
+    category_count = fields.Integer(compute='_compute_category_count')
+
+    @api.depends('category_ids')
+    def _compute_category_count(self):
+        for folder in self:
+            folder.category_count = len(folder.category_ids)
