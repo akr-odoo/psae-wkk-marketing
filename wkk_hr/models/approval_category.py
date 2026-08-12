56ee256182d2b82c66a9889118a85dbc5919d02b
ayek-odoo <ayek@odoo.com>
2026-08-12 10:14:01 +0400

    [ADD][6428115] wkk_hr: Add fields to apporval request from time off Update dates, description and attachments from time off to approval request on creation

--- wkk_hr/models/approval_category.py
+++ wkk_hr/models/approval_category.py
@@ -1,7 +1,19 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ApprovalCategory(models.Model):
     _inherit = "approval.category"
 
     approval_type = fields.Selection(selection_add=[("timeoff", "Time Off Approval")])
+
+    @api.onchange("approval_type")
+    def _onchange_approval_type_timeoff(self):
+        if self.approval_type == "timeoff":
+            self.has_period = "required"
+
+    @api.constrains("approval_type", "has_period")
+    def _check_timeoff_requires_period(self):
+        for category in self:
+            if category.approval_type == "timeoff" and category.has_period != "required":
+                raise ValidationError(_("Time Off approval categories must require a period."))
