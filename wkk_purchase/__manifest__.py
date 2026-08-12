f06c66888c2256933e85946abc648517eb80fa40
ayek-odoo <ayek@odoo.com>
2026-08-12 11:03:27 +0400

    [ADD][6431144] wkk_purchase: Add image field on POL and RFQ report

--- wkk_purchase/__manifest__.py
+++ wkk_purchase/__manifest__.py
@@ -4,11 +4,17 @@
     "author": "Odoo PS",
     "website": "https://www.odoo.com",
     "category": "Purchase/Purchase",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "license": "OEEL-1",
     "depends": ["wkk_approvals", "project_purchase"],
     "data": [
         "views/purchase_order_views.xml",
+        "report/purchase_quotation_templates.xml",
     ],
-    "task_ids": [5211287],
+    "assets": {
+        "web.report_assets_common": [
+            "wkk_purchase/static/src/scss/report.scss",
+        ],
+    },
+    "task_ids": [5211287, 6431144],
 }
